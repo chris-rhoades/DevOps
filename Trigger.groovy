39cf418d547f8c7a9bae74071eca8c6c8e28b72f
Zacarias Chironda <29487810+gladzeka@users.noreply.github.com>
2021-04-20 00:10:52 +0200

    Update Trigger.groovy

--- Trigger.groovy
+++ Trigger.groovy
@@ -18,7 +18,7 @@ agent {
               
               
 		        	sh 'python3 -m rflint --ignore LineTooLong myapp'
-		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --outputdir reports1 C:\Program Files\Python39\Scripts\MyWokspace'
+		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --outputdir reports1 Python39\Scripts\MyWokspace'
 		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --rerunfailed reports1/output.xml --outputdir Results SSH.robot'
 		        	sh 'python3 -m robot.rebot --merge --output reports/output.xml -l reports/log.html -r reports/report.html reports1/output.xml reports/output.xml'
 		        	sh 'exit 0'
