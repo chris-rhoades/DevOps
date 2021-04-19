1288eeb710c0d793131c232df76a2f3f7cf1acbe
Zacarias Chironda <29487810+gladzeka@users.noreply.github.com>
2021-04-20 00:21:26 +0200

    Update Trigger.groovy

--- Trigger.groovy
+++ Trigger.groovy
@@ -1,16 +1,12 @@
 agent {
       agent any 
   }
-  environment {
-    QA_SERVER = 'https://qa.application.com/'
-    CT_SERVER = 'http://ct.application.com/'
-
-  }
+ 
   stages {
 	    stage('intialize') {
 	      steps {
 	        sh 'echo "PATH= ${PATH}'
-	      }
+	    }  
 	    }
     
 	    stage('Run Robot Tests') {
@@ -24,26 +20,7 @@ agent {
 		        	//sh 'python3 -m robot.rebot --merge --output reports/output.xml -l reports/log.html -r reports/report.html reports1/output.xml reports/output.xml'
 		        	sh 'exit 0'
 	      		}
-        
-	      post {
-        	always {
-		        script {
-		          step(
-			            [
-			              $class              : 'RobotPublisher',
-			              outputPath          : 'reports',
-			              outputFileName      : '**/output.xml',
-			              reportFileName      : '**/report.html',
-			              logFileName         : '**/log.html',
-			              disableArchiveOutput: false,
-			              passThreshold       : 50,
-			              unstableThreshold   : 40,
-			              otherFiles          : "**/*.png,**/*.jpg",
-			            ]
-		          	)
-		        }
-	  		}		
-	    }
+         
 	}    
   }
   
