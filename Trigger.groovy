6f6bf6fa7c50d4452023d4ae1af6f4536fac97c6
Zacarias Chironda <29487810+gladzeka@users.noreply.github.com>
2021-04-20 00:04:32 +0200

    Call .robot through jenkinsfile
    Adding the Trigger robot framework file

---
+++ Trigger.groovy
@@ -0,0 +1,51 @@
+agent {
+      label 'qatest'
+  }
+  environment {
+    QA_SERVER = 'https://qa.application.com/'
+    CT_SERVER = 'http://ct.application.com/'
+
+  }
+  stages {
+	    stage('intialize') {
+	      steps {
+	        sh 'echo "PATH= ${PATH}'
+	      }
+	    }
+    
+	    stage('Run Robot Tests') {
+	      steps {
+              
+              sh ' c:'
+              sh 'cd C:\Program Files\Python39\Scripts\MyWokspace'
+              sh  'robot -d Results SSH.robot'
+		        	sh 'python3 -m rflint --ignore LineTooLong myapp'
+		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --outputdir reports1 C:\Program Files\Python39\Scripts\MyWokspace'
+		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --rerunfailed reports1/output.xml --outputdir reports myapp/uiTest/testCases/smokeSuite/'
+		        	sh 'python3 -m robot.rebot --merge --output reports/output.xml -l reports/log.html -r reports/report.html reports1/output.xml reports/output.xml'
+		        	sh 'exit 0'
+	      		}
+        
+	      post {
+        	always {
+		        script {
+		          step(
+			            [
+			              $class              : 'RobotPublisher',
+			              outputPath          : 'reports',
+			              outputFileName      : '**/output.xml',
+			              reportFileName      : '**/report.html',
+			              logFileName         : '**/log.html',
+			              disableArchiveOutput: false,
+			              passThreshold       : 50,
+			              unstableThreshold   : 40,
+			              otherFiles          : "**/*.png,**/*.jpg",
+			            ]
+		          	)
+		        }
+	  		}		
+	    }
+	}    
+  }
+  
+}
