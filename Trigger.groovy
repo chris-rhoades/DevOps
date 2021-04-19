679c090d6a4c6550287b6930172ca7318cf92715
Zacarias Chironda <29487810+gladzeka@users.noreply.github.com>
2021-04-20 00:09:22 +0200

    Update Trigger.groovy

--- Trigger.groovy
+++ Trigger.groovy
@@ -16,12 +16,10 @@ agent {
 	    stage('Run Robot Tests') {
 	      steps {
               
-              sh ' c:'
-              sh 'cd C:\Program Files\Python39\Scripts\MyWokspace'
-              sh  'robot -d Results SSH.robot'
+              
 		        	sh 'python3 -m rflint --ignore LineTooLong myapp'
 		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --outputdir reports1 C:\Program Files\Python39\Scripts\MyWokspace'
-		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --rerunfailed reports1/output.xml --outputdir reports myapp/uiTest/testCases/smokeSuite/'
+		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --rerunfailed reports1/output.xml --outputdir Results SSH.robot'
 		        	sh 'python3 -m robot.rebot --merge --output reports/output.xml -l reports/log.html -r reports/report.html reports1/output.xml reports/output.xml'
 		        	sh 'exit 0'
 	      		}
