54cccabc54bb250972dbe4093df03eea35d8e6ab
Zacarias Chironda <29487810+gladzeka@users.noreply.github.com>
2021-04-20 00:17:59 +0200

    Update Trigger.groovy

--- Trigger.groovy
+++ Trigger.groovy
@@ -15,13 +15,13 @@ agent {
     
 	    stage('Run Robot Tests') {
 	      steps {
-              
+              			sh 'c:'
              			sh 'C:/Program Files/Python39/Scripts/MyWokspace'
 		      		sh ' robot -d SSH.robot'
-		        	sh 'python3 -m rflint --ignore LineTooLong myapp'
-		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --outputdir reports1 C:/Program Files/Python39/Scripts/MyWokspace'
-		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --rerunfailed reports1/output.xml --outputdir C:/Program Files/Python39/Scripts/MyWokspace'
-		        	sh 'python3 -m robot.rebot --merge --output reports/output.xml -l reports/log.html -r reports/report.html reports1/output.xml reports/output.xml'
+		        	//sh 'python3 -m rflint --ignore LineTooLong myapp'
+		        	//sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --outputdir reports1 C:/Program Files/Python39/Scripts/MyWokspace'
+		        	//sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --rerunfailed reports1/output.xml --outputdir C:/Program Files/Python39/Scripts/MyWokspace'
+		        	//sh 'python3 -m robot.rebot --merge --output reports/output.xml -l reports/log.html -r reports/report.html reports1/output.xml reports/output.xml'
 		        	sh 'exit 0'
 	      		}
         
