4489563f63b28671400d68da81fb17f7a33a6d64
Zacarias Chironda <29487810+gladzeka@users.noreply.github.com>
2021-04-22 15:10:54 +0200

    Update TriggerTest.groovy

--- TriggerTest.groovy
+++ TriggerTest.groovy
@@ -30,7 +30,7 @@ pipeline {
 				bat 'robot -d Results TestSuite1.robot'
 
 /*  ############### Here We are trying to rerun the failed test cases of previous execution #############################*/
-				bat 'robot --rerunfailed  Results/output.xml --outputdir Scripts/MyWokspace '   
+				bat 'robot -d --rerunfailed  Results/output.xml --outputdir Scripts/MyWokspace '   
 	
  /* ############## In this line, We are merging the results of both executions and create a single report ############### */
 		        	bat 'python -m robot.rebot --merge --output reports/output.xml -l reports/log.html -r Results/report.html Results/output.xml Results/output.xml' 
