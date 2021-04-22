f35aa5866575af865ffadba61dd4fda5586c2949
Zacarias Chironda <29487810+gladzeka@users.noreply.github.com>
2021-04-22 11:21:09 +0200

    Update Trigger.groovy

--- Trigger.groovy
+++ Trigger.groovy
@@ -8,14 +8,15 @@ pipeline {
 
   }
   stages {
-	    stage('intialize') { //In above code, we just printing the system path details. just to use in case of failure and as helper details to troubleshoot.
+	    stage('Intialize Path') { //In above code, we just printing the system path details. just to use in case of failure and as helper details to troubleshoot.
 	      steps {
 	        sh 'echo "PATH= ${PATH}'
 	      }
 	    }
     
-	    stage('Run Robot Tests') {
-	      steps {
+	    stage('Run Robot Framework Tests') {
+	      steps {		sh 'c:'
+		                sh 'C:/Program Files/Python39/Scripts/MyWokspace/'
 		        	sh 'python3 -m rflint --ignore LineTooLong myapp' // In this line, we are running robot framework lint to check for Programmatic as well as Stylistic errors on our code base
 		        	sh 'python3 -m robot.run --NoStatusRC --variable SERVER:${CT_SERVER} --outputdir reports1 C:/Program Files/Python39/Scripts/MyWokspace/' //In this line, we are basically running the robot automation testcases available on folder  C:\Program Files\Python39\Scripts\MyWokspace and save execution results to report1
 		        	sh 'robot -d Results TestSuite1.robot'
