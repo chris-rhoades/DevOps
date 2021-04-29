fbbecb5ae89c2930b54946f36f2d023c1a05999b
Zacarias Chironda <29487810+gladzeka@users.noreply.github.com>
2021-04-29 16:07:34 +0200

    Update TriggerTest.groovy

--- TriggerTest.groovy
+++ TriggerTest.groovy
@@ -79,6 +79,6 @@ stage(' Path') { //In above code, we just printing the system path details. just
 }
 def get_first() {
     node('main') {
-        return  'c:/Program Files/Python39/Scripts/MyWokspace'
+        return 'path = "${workspace}/pic_env_vars.properties'
     }
 }
